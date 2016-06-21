33fa0dd835eab497b8fa6005c375e71afe1fd64f
sh4 <sh4@users.noreply.github.com>
2016-06-21 23:13:27 +0900

    Add unit tests.

--- saya.test/UnitTest1.cs
+++ saya.test/UnitTest1.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using saya.core;
+using Shell32;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace saya.test
 {
@@ -59,6 +63,124 @@ namespace saya.test
         private static void AreScoreEqual(string text, string abbrevaition, float expectedScore, float delta = 0.0001f)
         {
             Assert.AreEqual(AlcorAbbreviationScorer.Compute(text, abbrevaition), expectedScore, delta);
+        }
+    }
+
+    static class ShortcutUtils
+    {
+        public static IEnumerable<string> GetLocalShortcuts()
+        {
+            var startMenuPaths = new[] {
+                Environment.GetFolderPath(Environment.SpecialFolder.StartMenu),
+                Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu),
+            };
+            foreach (var startMenuDir in startMenuPaths)
+            {
+                foreach (var path in FileUtils.EnumerateFileEntries(startMenuDir))
+                {
+                    if ((Path.GetExtension(path) ?? string.Empty).Equals(".lnk", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        yield return path;
+                    }
+                }
+            }
+
+        }
+    }
+
+    [TestClass]
+    public class ShortcutParserTest
+    {
+        [TestMethod]
+        public void LocalFileShortcut()
+        {
+            foreach (var path in ShortcutUtils.GetLocalShortcuts())
+            {
+                AreEqualShortcut(path);
+            }
+        }
+
+        private static void AreEqualShortcut(string shortcutPath)
+        {
+            Assert.IsTrue(File.Exists(shortcutPath), $"File not found: {shortcutPath}");
+            try
+            {
+                var expected = GetShell32LinkPath(shortcutPath);
+                var actual = FileUtils.GetShortcutPath(shortcutPath) ?? string.Empty;
+                Assert.AreEqual(expected, actual, shortcutPath);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                var actual = FileUtils.GetShortcutPath(shortcutPath);
+                if (actual != null)
+                {
+                    if (File.Exists(actual))
+                    {
+                    }
+                    else if (Directory.Exists(actual))
+                    {
+                    }
+                    else
+                    {
+                        Assert.Fail($"Shortcut target file is not found: {actual} (Shortcut file is '{shortcutPath}')");
+                    }
+                }
+            }
+        }
+
+        private static string GetShell32LinkPath(string shortcutPath)
+        {
+            var directoryPath = Path.GetDirectoryName(shortcutPath);
+            var fileName = Path.GetFileName(shortcutPath);
+            var shell = new Shell();
+            var namespaceObj = shell.NameSpace(directoryPath);
+            var shortcutObj = namespaceObj.ParseName(fileName);
+            return shortcutObj.GetLink.Path;
+        }
+    }
+
+
+    [TestClass]
+    public class ShortcutParsePerformanceTeset
+    {
+        private static readonly TimeSpan NeedForPerformanceTuningTime = TimeSpan.FromMilliseconds(100);
+
+        [TestMethod]
+        public void CompareParsePerformanceTest()
+        {
+            var elapsed = ElapsedTime(() =>
+            {
+                foreach (var path in ShortcutUtils.GetLocalShortcuts())
+                {
+                    FileUtils.GetShortcutPath(path);
+                }
+            });
+
+            var shell32LinkParserElapsed = ElapsedTime(() =>
+            {
+                var shell32 = new Shell();
+                foreach (var path in ShortcutUtils.GetLocalShortcuts())
+                {
+                    var directoryPath = Path.GetDirectoryName(path);
+                    var fileName = Path.GetFileName(path);
+                    try
+                    {
+                        var targetPath = shell32.NameSpace(directoryPath).ParseName(fileName).GetLink.Path;
+                    }
+                    catch (UnauthorizedAccessException) { }
+                }
+            });
+
+            Assert.IsTrue(elapsed < shell32LinkParserElapsed, $"Elapsed time: {elapsed.Milliseconds}ms");
+            Assert.IsTrue(elapsed < NeedForPerformanceTuningTime, $"Elapsed time: {elapsed.Milliseconds}ms");
+        }
+
+        private static TimeSpan ElapsedTime(Action action)
+        {
+            var stopWatch = Stopwatch.StartNew();
+            action();
+            stopWatch.Stop();
+            return TimeSpan.FromTicks(stopWatch.ElapsedTicks);
         }
     }
 }
